b9ec45de7fac179eab66e4d9ab8fd446c3dc02aa
bonomences-jpg <bonomences@gmail.com>
2026-07-22 10:31:05 +0700

    prepare for render deployment

--- server.js
+++ server.js
@@ -165,6 +165,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 
-    console.log(`Server running : http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 
 });
